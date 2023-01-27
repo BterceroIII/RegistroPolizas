59a29ee19d28d134e8a50e896bef9d103a97b8ed
Byron Tercero <B1tercero.cruz@gmail.com>
2023-01-27 15:51:54 -0600

    added "Employees" controller and minor changes to "_Layout"

---
+++ ProyectoPoliza/Controllers/EmpleadosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoPoliza.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
