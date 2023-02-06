6d9753939870171b7f0558738f81585d0d575a52
Byron Tercero <B1tercero.cruz@gmail.com>
2023-02-05 19:38:45 -0600

    Recursos: Create class Utility to encrypt passwords

---
+++ ProyectoPoliza/Recursos/Utilidades.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ProyectoPoliza.Recursos
+{
+    public class Utilidades
+    {
+        public static string EncriptarClave(string clave)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            using (SHA256 has = SHA256.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+
+                byte[] result = has.ComputeHash(enc.GetBytes(clave));
+
+                foreach (byte b in result )
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
