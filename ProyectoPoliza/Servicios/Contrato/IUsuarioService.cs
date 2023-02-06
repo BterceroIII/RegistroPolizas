adb6abac87247b48a54d77e173efc84a0ead75e6
Byron Tercero <B1tercero.cruz@gmail.com>
2023-02-05 19:33:21 -0600

    Servicios: creation of IUsuarioService and UsuarioServicie

---
+++ ProyectoPoliza/Servicios/Contrato/IUsuarioService.cs
@@ -0,0 +1,10 @@
+﻿using ProyectoPoliza.Models;
+
+namespace ProyectoPoliza.Servicios.Contrato
+{
+    public interface IUsuarioService
+    {
+        Task<Usuario> GetUsuario(String correo, string clave);
+        Task<Usuario> SaveUsuario(Usuario modelo);
+    }
+}
---
+++ ProyectoPoliza/Servicios/Implementacion/UsuarioService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoPoliza.Models;
+using ProyectoPoliza.Servicios.Contrato;
+
+namespace ProyectoPoliza.Servicios.Implementacion
+{
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly GestionSegurosSaContext _SeguroContext;
+
+        public UsuarioService(GestionSegurosSaContext seguroContext)
+        {
+            _SeguroContext = seguroContext;
+        }
+
+        public async Task<Usuario> GetUsuario(string correo, string clave)
+        {
+            Usuario usuario_encontrado = await _SeguroContext.Usuarios.Where(u => u.Correo == correo && u.Contraseña == clave)
+                .FirstOrDefaultAsync();
+
+            return usuario_encontrado;
+        }
+
+        public async Task<Usuario> SaveUsuario(Usuario modelo)
+        {
+            _SeguroContext.Usuarios.Add(modelo);
+            await _SeguroContext.SaveChangesAsync();
+
+            return modelo;
+        }
+    }
+}
