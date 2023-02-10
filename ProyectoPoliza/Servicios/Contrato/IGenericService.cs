c4f626ec177c05316022e336dfbe871353618a89
Byron Tercero <B1tercero.cruz@gmail.com>
2023-02-09 21:23:33 -0600

    Contrato: generic interface added

---
+++ ProyectoPoliza/Servicios/Contrato/IGenericService.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoPoliza.Servicios.Contrato
+{
+    public interface IGenericService<T> where T : class
+    {
+        Task<List<T>> List();
+        Task<bool>  Save(T model);
+        Task<bool>  Edit(T model);
+        Task<bool>  Delete(int id);
+    }
+}
