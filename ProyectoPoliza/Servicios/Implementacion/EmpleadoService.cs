70d809c7207b121b41698b80fbce52f8b751cb4c
Byron Tercero <B1tercero.cruz@gmail.com>
2023-02-09 23:36:14 -0600

    Servicios: CRUD Services Employees

---
+++ ProyectoPoliza/Servicios/Implementacion/EmpleadoService.cs
@@ -0,0 +1,127 @@
+﻿using ProyectoPoliza.Models;
+using ProyectoPoliza.Servicios.Contrato;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ProyectoPoliza.Servicios.Implementacion
+{
+    public class EmpleadoService : IGenericService<Empleado>
+    {
+        private readonly string _cadenaSQL = "";
+
+        public EmpleadoService(IConfiguration configuration)
+        {
+            _cadenaSQL = configuration.GetConnectionString("cadenaSQL");
+        }
+        public async Task<List<Empleado>> List()
+        {
+            List<Empleado> _lista = new List<Empleado>();
+            using (var conexion = new SqlConnection(_cadenaSQL))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("Mostrar_Empleados", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = await cmd.ExecuteReaderAsync())
+                {
+                    while (await dr.ReadAsync())
+                    {
+                        _lista.Add(new Empleado
+                        {
+                            IdEmpleado = Convert.ToInt32(dr["idEmpleado"]),
+                            Nombre = dr["Nombre"].ToString(),
+                            Cedula = dr["Cedula"].ToString(),
+                            Telefono = dr["Telefono"].ToString(),
+                            Cargo = dr["Cargo"].ToString(),
+                            Eliminado = Convert.ToInt32(dr["Eliminado"])
+
+                        });
+                    }
+                }
+            }
+
+            return _lista;
+
+        }
+        public async Task<bool> Save(Empleado model)
+        {
+            using (var conexion = new SqlConnection(_cadenaSQL))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("Insertar_Empleado", conexion);
+                cmd.Parameters.AddWithValue("Nombre", model.Nombre);
+                cmd.Parameters.AddWithValue("Cedula", model.Cedula);
+                cmd.Parameters.AddWithValue("Telefono", model.Telefono);
+                cmd.Parameters.AddWithValue("Cargo", model.Cargo);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                int filas_afectadas = await cmd.ExecuteNonQueryAsync();
+
+                if (filas_afectadas > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+        }
+
+        public async Task<bool> Edit(Empleado model)
+        {
+            using (var conexion = new SqlConnection(_cadenaSQL))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("Modificar_Empleados", conexion);
+                cmd.Parameters.AddWithValue("idEmpleado", model.IdEmpleado);
+                cmd.Parameters.AddWithValue("Nombre", model.Nombre);
+                cmd.Parameters.AddWithValue("Cedula", model.Cedula);
+                cmd.Parameters.AddWithValue("Telefono", model.Telefono);
+                cmd.Parameters.AddWithValue("Cargo", model.Cargo);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                int filas_afectadas = await cmd.ExecuteNonQueryAsync();
+
+                if (filas_afectadas > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            using (var conexion = new SqlConnection(_cadenaSQL))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("sp_EliminarEmpleado", conexion);
+                cmd.Parameters.AddWithValue("idEmpleado", id);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                int filas_afectadas = await cmd.ExecuteNonQueryAsync();
+
+                if (filas_afectadas > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
+
+
+        }
+    }
+}
